5e61d603564f5735a831039412c2a9d70a791ebb
summerUniq <1152972727@qq.com>
2020-09-15 15:50:58 +0800

    react-router的使用

---
+++ src/pages/RouterTest.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Link, Route} from 'react-router-dom'
+
+
+function ProductList(props) {
+    return <div>ProductList</div>;
+  }
+  function ProductMgt(props) {
+    return <div>ProductMgt</div>;
+  }
+
+  function Detail({match, history, location}) {
+      console.log(match, history, location);
+      return (
+        <div>
+           ProductMgt
+        <p>{match.params.name}</p> </div>
+      )
+  }
+
+class RouterTest extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <BrowserRouter>
+                <nav>
+                    <Link to="/">商品列表</Link>
+                    <Link to="/management">商品管理</Link>
+                    <Link to="/detail/web">web全栈</Link>
+                </nav>
+                <div>
+                    <Route exact path="/" component={ProductList}/>
+                    <Route path="/management" component={ProductMgt}/>
+                    <Route path="/detail/:name" component={Detail}/>
+                </div>
+            </BrowserRouter>
+         );
+    }
+}
+ 
+export default RouterTest;
